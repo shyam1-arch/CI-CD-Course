1efeda41cbf20700c713baf7115f46f372020fea
shyam1-arche <shyamyadav0816.sy@gmail.com>
2026-05-10 02:42:21 +0300

    fix eslint globals

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
 
-
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
 const start = async () => {
   await app.listen(PORT)
-  
-    console.log(`server started on port ${PORT}`)
+  console.log(`server started on port ${PORT}`)
 }
 
 start()
