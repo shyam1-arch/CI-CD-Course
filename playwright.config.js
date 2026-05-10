246d75f621a85a542e91617ff993fe03a89e626d
shyam1-arche <shyamyadav0816.sy@gmail.com>
2026-05-10 03:46:23 +0300

    add playwright workflow steps

---
+++ playwright.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@playwright/test')
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  timeout: 30000,
+  use: {
+    baseURL: 'http://localhost:8080'
+  },
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI
+  }
+})
